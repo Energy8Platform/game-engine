b7677c3b10aacd0cf15e7f21fe97c4eeb0b3684a
Maksim Melnikov <melnikovmv@bk.ru>
2026-06-22 14:32:19 +0300

    feat(stake-kit): canonical enrichConfigWithJurisdiction

--- packages/stake-kit/src/index.ts
+++ packages/stake-kit/src/index.ts
@@ -1,2 +1,4 @@
 export * from './book';
 export { deriveArrayFields } from './schema';
+export { enrichConfigWithJurisdiction } from './jurisdiction';
+export type { RegulatoryMapping } from './jurisdiction';
